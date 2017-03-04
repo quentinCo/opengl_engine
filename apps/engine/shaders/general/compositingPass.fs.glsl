064703ac3f04755daf0f2ad076bd66944ebdc0bd
quentinCo <quentin.couillard@gmail.com>
2017-03-04 16:27:23 +0100

    *chg: decrease number of layer

--- apps/engine/shaders/general/compositingPass.fs.glsl
+++ apps/engine/shaders/general/compositingPass.fs.glsl
@@ -8,12 +8,6 @@ uniform sampler2D uLayer0;
 uniform sampler2D uLayer1;
 uniform sampler2D uLayer2;
 uniform sampler2D uLayer3;
-uniform sampler2D uLayer4;
-uniform sampler2D uLayer5;
-uniform sampler2D uLayer6;
-uniform sampler2D uLayer7;
-uniform sampler2D uLayer8;
-uniform sampler2D uLayer9;
 
 out vec4 fColor;
 
@@ -24,10 +18,4 @@ void main()
 	fColor += texelFetch(uLayer1 , ivec2(gl_FragCoord.xy), 0);
 	fColor += texelFetch(uLayer2 , ivec2(gl_FragCoord.xy), 0);
 	fColor += texelFetch(uLayer3 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer4 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer5 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer6 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer7 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer8 , ivec2(gl_FragCoord.xy), 0);
-	fColor += texelFetch(uLayer9 , ivec2(gl_FragCoord.xy), 0);
 }
