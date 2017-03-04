6c4c27387539fa1e43a67bd83f42fb6a92ac684d
quentinCo <quentin.couillard@gmail.com>
2017-03-04 16:28:33 +0100

    *chg: clean emissive shader

--- apps/engine/shaders/general/emissiveElement.fs.glsl
+++ apps/engine/shaders/general/emissiveElement.fs.glsl
@@ -2,8 +2,6 @@
 
 layout(location = 0) out vec3 fEmissive;
 
-//in vec3 viewSpacePosition;
-
 uniform vec3 uKe;
 
 void main()
--- apps/engine/shaders/general/emissiveElement.vs.glsl
+++ apps/engine/shaders/general/emissiveElement.vs.glsl
@@ -2,13 +2,9 @@
 
 layout(location = 0) in vec3 aPosition;
 
-//out vec3 viewSpacePosition;
-
 uniform mat4 uModelViewProjMatrix;
-uniform mat4 uModelViewMatrix;
 
 void main()
 {
-	//viewSpacePosition = vec3(uModelViewMatrix * vec4(aPosition, 1));
 	gl_Position = uModelViewProjMatrix * vec4(aPosition, 1);
 }
