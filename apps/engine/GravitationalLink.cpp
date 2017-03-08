1a1102163e0835ce15c85907830112ee9be7b4c0
quentinCo <quentin.couillard@gmail.com>
2017-03-08 01:12:56 +0100

    *add : GravitationalLink implementation

---
+++ apps/engine/GravitationalLink.cpp
@@ -0,0 +1,19 @@
+#include "GravitationalLink.hpp"
+
+using namespace qc::physic;
+
+const float GravitationalLink::gConst = static_cast<float>( 6.67384 * pow(10, -11));
+
+void GravitationalLink::update()
+{
+	glm::vec3 direction = object2->getPosition() - object1->getPosition();
+	float distance = static_cast<float>(direction.length());
+	float minDist = object2->getRadiusAttraction() + object1->getRadiusAttraction();
+	if (distance > minDist)
+		return;
+	direction = glm::normalize(direction);
+	glm::vec3 force = -gConst * object1->getMass() * object2->getMass() * direction / (distance * distance);
+
+	object1->addToForce(force);
+	object2->addToForce(-force);
+}
